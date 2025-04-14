366ca04385f4e0be646edb7e07ed5b72cf38da50
dipishBisht <dipish1718@gmail.com>
2025-04-14 07:43:35 +0530

    fix: build error fixed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Cta from "@/components/home/cta";
 import Faq from "@/components/home/faq";
 import HomeHero from "@/components/home/hero";
 import HowItWorks from "@/components/home/how-it-works";
-import Partners from "@/components/home/partners";
+// import Partners from "@/components/home/partners";
 import Pricing from "@/components/home/pricing";
 // import Testimonials from "@/components/home/testimonials";
 import WhyChooseUs from "@/components/home/why-choose-us";
